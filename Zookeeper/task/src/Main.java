6b704552ee269b9b7a242e44f9754edccc9174e3
vrnsky <vrnsky@protonmail.com>
2023-01-10 21:39:26 +0800

    initial commit

---
+++ Zookeeper/task/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String poem = """
+              "Over hill, over dale,
+              Thorough bush, thorough brier,
+              Over park, over pale,
+              Thorough flood, thorough fire!\"""";
+
+        System.out.println(poem);
+        System.out.print("test");
+    }
+}
